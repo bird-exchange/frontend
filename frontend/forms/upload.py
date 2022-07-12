a9b4ff8ba978d1e94b29fbe5945749f1883ec54a
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-12 22:17:42 +0500

    bugfix: upload url

--- frontend/forms/upload.py
+++ frontend/forms/upload.py
@@ -5,7 +5,7 @@ from werkzeug.utils import secure_filename
 from frontend.config import config
 
 
-upload_url = f'{config.endpoint}/upload/'
+upload_url = f'{config.endpoint}/files/'
 
 
 class UploadFileForm(FlaskForm):
