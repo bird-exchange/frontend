bce4c4daa28d76449cfc97d52fba1f78254e12eb
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-24 23:06:57 +0500

    feature: index view return template without pars

--- frontend/view/index.py
+++ frontend/view/index.py
@@ -1,15 +1,8 @@
 from flask import Blueprint, render_template
-from frontend.forms.upload import UploadFileForm
-
 
 view = Blueprint("index", __name__)
 
 
-@view.route('/', methods=['GET', 'POST'])
+@view.route('/')
 def index():
-    form = UploadFileForm(meta={'csrf': False})
-
-    if form.validate_on_submit():
-        form.upload_file()
-
-    return render_template('index.html', form=form)
+    return render_template('index.html')
