0093aeb13cfc0750f8a4df9c86184bf8382be2aa
ProtKsen <protcenko.kseniya@gmail.com>
2022-08-02 19:13:33 +0500

    feature: add kind to upload

--- frontend/forms/upload.py
+++ frontend/forms/upload.py
@@ -2,8 +2,8 @@ import httpx
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField, FileRequired
 from werkzeug.utils import secure_filename
-from frontend.config import config
 
+from frontend.config import config
 
 upload_url = f'{config.endpoint}/files/'
 
@@ -17,11 +17,12 @@ class UploadFileForm(FlaskForm):
         ]
     )
 
-    def upload_file(self) -> bool:
+    def upload_file(self, kind: str) -> bool:
         file = self.file.data
         filename = secure_filename(file.filename)
         files = {'file': (filename, file)}
+        data = {'kind': kind}
         timeout = httpx.Timeout(5.0, connect=5.0, read=50.0, write=5.0)
-        answer = httpx.post(upload_url, files=files, timeout=timeout)
+        answer = httpx.post(upload_url, files=files, params=data, timeout=timeout)
 
         return answer.status_code == 201
--- frontend/view/upload.py
+++ frontend/view/upload.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, render_template
-from frontend.forms.upload import UploadFileForm
 
+from frontend.forms.upload import UploadFileForm
 
 view = Blueprint("upload", __name__)
 
 
-@view.route('/', methods=['GET', 'POST'])
-def upload():
+@view.route('/<string:kind>/', methods=['GET', 'POST'])
+def upload(kind: str):
     form = UploadFileForm(meta={'csrf': False})
 
     if form.validate_on_submit():
-        form.upload_file()
+        form.upload_file(kind=kind)
 
-    return render_template('upload.html', form=form)
+    return render_template('upload.html', form=form, kind=kind)
