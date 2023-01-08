37aff4131fec7017dc1dc440e78017ce0da866b0
ProtKsen <protcenko.kseniya@gmail.com>
2023-01-08 15:24:12 +0500

    hotfix add port and host

--- frontend/__main__.py
+++ frontend/__main__.py
@@ -1,5 +1,7 @@
 import logging
 
+import config
+
 from frontend.app import create_app
 
 logger = logging.getLogger(__name__)
@@ -10,7 +12,7 @@ def main():
     logger.info('application started')
 
     app = create_app()
-    app.run()
+    app.run(host=config.host, port=config.port, debug=False)
 
 
 if __name__ == "__main__":
--- frontend/config.py
+++ frontend/config.py
@@ -5,11 +5,15 @@ from pydantic import BaseModel
 
 class AppConfig(BaseModel):
     endpoint: str
+    host: str
+    port: int
 
 
 def load_from_env() -> AppConfig:
     endpoint = os.environ['ENDPOINT']
-    return AppConfig(endpoint=endpoint)
+    host = os.environ['APP_HOST']
+    port = int(os.environ['APP_PORT'])
+    return AppConfig(endpoint=endpoint, host=host, port=port)
 
 
 config = load_from_env()
